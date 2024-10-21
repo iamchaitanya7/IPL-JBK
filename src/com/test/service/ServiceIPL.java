5c4bd84ae4900067edc505a1b3ea084c9d9db79a
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-10-21 18:38:56 +0530

    Update ServiceIPL.java

--- src/com/test/service/ServiceIPL.java
+++ src/com/test/service/ServiceIPL.java
@@ -1,4 +1,31 @@
 package com.test.service;
 
+import com.test.dao.DaoIPL;
+import com.test.entity.PlayersIPL;
+import java.util.List;
+
 public class ServiceIPL {
+    private DaoIPL daoIPL = null;
+
+    public ServiceIPL() {
+        //daoIPL = new DaoIPL();
+    }
+
+    // Method to return all players
+    public List<PlayersIPL> fetchAllPlayers() {
+        daoIPL = new DaoIPL();
+        return daoIPL.getAllPlayers();
+    }
+
+    // Method to add a player
+    public void registerPlayer(PlayersIPL player) {
+        daoIPL = new DaoIPL();
+        daoIPL.addPlayer(player);
+    }
+
+    // Method to find a player by jersey number
+    public PlayersIPL findPlayerByJerseyNo(int jerseyNo) {
+        daoIPL = new DaoIPL();
+        return daoIPL.getPlayerByJerseyNo(jerseyNo);
+    }
 }
