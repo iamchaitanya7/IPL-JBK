6637796397b5ac459aebae6f6f9942ac5a18a1bc
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-10-21 18:48:59 +0530

    Update TestIPL.java

--- src/com/test/TestIPL.java
+++ src/com/test/TestIPL.java
@@ -3,8 +3,6 @@ package com.test;
 import com.test.controller.ControllerIPL;
 
 public class TestIPL {
-
-
     public static void main (String[] args) {
         ControllerIPL controllerIPL = new ControllerIPL ();
         controllerIPL.startIPL();
