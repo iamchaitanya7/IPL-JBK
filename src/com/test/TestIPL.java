784de3bb5f8fd3771b0e99d4a830b49c00ce13ca
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-10-21 18:18:00 +0530

    InitialC

--- src/com/test/TestIPL.java
+++ src/com/test/TestIPL.java
@@ -1,9 +1,11 @@
 package com.test;
 
+import com.test.controller.ControllerIPL;
+
 public class TestIPL {
 
 
     public static void main (String[] args) {
-
+        ControllerIPL controllerIPL = new ControllerIPL ();
     }
 }
