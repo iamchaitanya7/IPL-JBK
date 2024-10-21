8c39838dbfdf2c635bb845319b77d8a30915b053
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-10-21 18:38:30 +0530

    Update PlayersIPL.java

--- src/com/test/entity/PlayersIPL.java
+++ src/com/test/entity/PlayersIPL.java
@@ -4,58 +4,60 @@ public class PlayersIPL {
     private int jerseyNo;
     private String name;
     private int runs;
-    private double strikerate;
+    private int wickets;
+    private double strikeRate;
 
-    public PlayersIPL(){
-
-    }
-
-    public PlayersIPL (int jerseyNo, String name, int runs, double strikerate) {
+    public PlayersIPL(int jerseyNo, String name, int runs, int wickets, double strikeRate) {
         this.jerseyNo = jerseyNo;
         this.name = name;
         this.runs = runs;
-        this.strikerate = strikerate;
+        this.wickets = wickets;
+        this.strikeRate = strikeRate;
     }
 
-    public int getJerseyNo () {
+    public int getJerseyNo() {
         return jerseyNo;
     }
 
-    public void setJerseyNo (int jerseyNo) {
+    public void setJerseyNo(int jerseyNo) {
         this.jerseyNo = jerseyNo;
     }
 
-    public String getName () {
+    public String getName() {
         return name;
     }
 
-    public void setName (String name) {
+    public void setName(String name) {
         this.name = name;
     }
 
-    public int getRuns () {
+    public int getRuns() {
         return runs;
     }
 
-    public void setRuns (int runs) {
+    public void setRuns(int runs) {
         this.runs = runs;
     }
 
-    public double getStrikerate () {
-        return strikerate;
+    public int getWickets() {
+        return wickets;
     }
 
-    public void setStrikerate (double strikerate) {
-        this.strikerate = strikerate;
+    public void setWickets(int wickets) {
+        this.wickets = wickets;
+    }
+
+    public double getStrikeRate() {
+        return strikeRate;
+    }
+
+    public void setStrikeRate(double strikeRate) {
+        this.strikeRate = strikeRate;
     }
 
     @Override
-    public String toString () {
-        return "PlayersIPL{" +
-                "jerseyNo=" + jerseyNo +
-                ", name='" + name + '\'' +
-                ", runs=" + runs +
-                ", strikerate=" + strikerate +
-                '}';
+    public String toString() {
+        return "Player [Jersey No=" + jerseyNo + ", Name=" + name + ", Runs=" + runs + ", Wickets=" + wickets +
+                ", Strike Rate=" + strikeRate + "]";
     }
 }
