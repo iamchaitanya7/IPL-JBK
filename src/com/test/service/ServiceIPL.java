870c721e03db17c990b795496d0012514ad7ed82
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-10-22 11:19:19 +0530

    Update ServiceIPL.java

--- src/com/test/service/ServiceIPL.java
+++ src/com/test/service/ServiceIPL.java
@@ -2,6 +2,8 @@ package com.test.service;
 
 import com.test.dao.DaoIPL;
 import com.test.entity.PlayersIPL;
+
+import java.util.HashMap;
 import java.util.List;
 
 public class ServiceIPL {
@@ -11,21 +13,39 @@ public class ServiceIPL {
         //daoIPL = new DaoIPL();
     }*/
 
-    // Method to return all players
+    //Method to return all players.
     public List<PlayersIPL> fetchAllPlayers() {
         daoIPL = new DaoIPL();
         return daoIPL.getAllPlayers();
     }
 
-    // Method to add a player
+    //Method to add a player.
     public void registerPlayer(PlayersIPL player) {
         daoIPL = new DaoIPL();
         daoIPL.addPlayer(player);
     }
 
-    // Method to find a player by jersey number
+    //Method to find a player by jersey number.
     public PlayersIPL findPlayerByJerseyNo(int jerseyNo) {
         daoIPL = new DaoIPL();
         return daoIPL.getPlayerByJerseyNo(jerseyNo);
+    }
+
+    //Get all players by all team.
+    public HashMap<String, List<PlayersIPL>> getAllPlayersByTeam() {
+        daoIPL = new DaoIPL();
+        return daoIPL.getAllPlayersByTeam();
+    }
+
+    //Get players by a specific team.
+    public List<PlayersIPL> getPlayersByTeam(String team) {
+        daoIPL = new DaoIPL();
+        return daoIPL.getPlayersByTeam(team);
+    }
+
+    //Add a player to a specific team.
+    public void addPlayerToTeam(String team, PlayersIPL player) {
+        daoIPL = new DaoIPL();
+        daoIPL.addPlayerToTeam(team, player);
     }
 }
